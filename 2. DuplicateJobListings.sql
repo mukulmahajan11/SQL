6a19e85ae48e1aba07656a9f8399b57d208cd53b
Mukul Mahajan <162844950+mukulmahajan11@users.noreply.github.com>
2026-02-01 15:13:12 -0800

    Update 2. DuplicateJobListings.sql

--- 2. DuplicateJobListings.sql
+++ 2. DuplicateJobListings.sql
@@ -1,11 +1,10 @@
 WITH CTE_job AS (
-SELECT company_id, title, description, COUNT(job_id) AS job_count 
+SELECT company_id, title, description, COUNT(DISTINCT job_id) AS job_count 
 FROM job_listings
 GROUP BY company_id, title, description
+HAVING COUNT(DISTINCT job_id) > 1
 )
 
 SELECT COUNT(DISTINCT company_id) AS duplicate_companies 
 
-FROM CTE_job
-
-WHERE job_count>1;
+FROM CTE_job;
