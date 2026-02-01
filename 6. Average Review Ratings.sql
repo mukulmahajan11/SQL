7ae4b1aa648fb0f6d2de121c4e674dfd92c03ec5
Mukul Mahajan <162844950+mukulmahajan11@users.noreply.github.com>
2026-02-01 15:36:44 -0800

    Create 6. Average Review Ratings.sql

---
+++ 6. Average Review Ratings.sql
@@ -0,0 +1,13 @@
+SELECT 
+
+EXTRACT(MONTH FROM submit_date) AS mth, 
+product_id AS product, 
+ROUND(AVG(stars), 2) AS avg_stars
+
+FROM reviews
+
+GROUP BY product_id, EXTRACT(MONTH FROM submit_date)
+
+ORDER BY EXTRACT(MONTH FROM submit_date), product_id
+
+;
