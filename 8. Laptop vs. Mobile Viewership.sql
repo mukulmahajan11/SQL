abbf5be95a77a847e7ca25d7a1de4a28da205cdb
Mukul Mahajan <162844950+mukulmahajan11@users.noreply.github.com>
2026-02-01 16:00:38 -0800

    Update 8. Laptop vs. Mobile Viewership.sql

--- 8. Laptop vs. Mobile Viewership.sql
+++ 8. Laptop vs. Mobile Viewership.sql
@@ -4,3 +4,18 @@ SUM(CASE WHEN device_type = 'laptop' THEN 1 ELSE 0 END) AS laptop_views,
 SUM(CASE WHEN device_type IN ('tablet', 'phone') THEN 1 ELSE 0 END) AS mobile_views 
 
 FROM viewership;
+
+
+
+
+------------------SOLUTION WITH CTE
+
+
+WITH cte AS (SELECT 
+CASE WHEN device_type = 'laptop' THEN 1 ELSE 0 END AS laptop_views, 
+CASE WHEN device_type IN ('tablet', 'phone') THEN 1 ELSE 0 END AS mobile_views 
+FROM viewership)
+
+SELECT SUM(laptop_views), SUM(mobile_views) FROM cte
+
+;
