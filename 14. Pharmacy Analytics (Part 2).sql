76df7a28bb7bd2699767488406c6c242115e887c
Mukul Mahajan <162844950+mukulmahajan11@users.noreply.github.com>
2026-02-01 18:04:19 -0800

    Create 14. Pharmacy Analytics (Part 2).sql

---
+++ 14. Pharmacy Analytics (Part 2).sql
@@ -0,0 +1,12 @@
+SELECT manufacturer, COUNT(drug) AS drug_count, 
+
+ABS(SUM(total_sales - cogs)) AS total_loss
+
+FROM pharmacy_sales
+
+WHERE cogs > total_sales
+
+GROUP BY manufacturer
+
+ORDER BY total_loss DESC
+;
