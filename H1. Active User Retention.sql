c7881d161d8b555484aacbcc7aafaa49c2fde1cc
Mukul Mahajan <162844950+mukulmahajan11@users.noreply.github.com>
2026-02-02 19:32:58 -0800

    Update H1. Active User Retention.sql

--- H1. Active User Retention.sql
+++ H1. Active User Retention.sql
@@ -17,3 +17,22 @@ WHERE EXISTS
   AND EXTRACT(YEAR FROM curr_month.event_date) = 2022
 GROUP BY EXTRACT(MONTH FROM curr_month.event_date);
 
+
+-----------Alternative Solution (But NOT as Efficient as previous Solution)-------------------
+WITH cte AS (
+    SELECT DISTINCT user_id
+    FROM user_actions
+    WHERE EXTRACT(MONTH FROM event_date) = 7
+      AND EXTRACT(YEAR FROM event_date) = 2022
+
+    INTERSECT
+
+    SELECT DISTINCT user_id
+    FROM user_actions
+    WHERE EXTRACT(MONTH FROM event_date) = 6
+      AND EXTRACT(YEAR FROM event_date) = 2022
+)
+
+SELECT '7' AS month,
+       COUNT(user_id) AS monthly_active_user
+FROM cte;
