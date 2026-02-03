2df42a07c67046278490f6704f8dc1cfcea7e345
Mukul Mahajan <162844950+mukulmahajan11@users.noreply.github.com>
2026-02-02 17:20:56 -0800

    Update M9. Supercloud Customer.sql

--- M9. Supercloud Customer.sql
+++ M9. Supercloud Customer.sql
@@ -1,1 +1,12 @@
+WITH cte AS(
+SELECT c.*, p.product_category
+FROM customer_contracts c 
+LEFT JOIN products p 
+ON c.product_id = p.product_id)
 
+SELECT customer_id
+FROM cte 
+GROUP BY customer_id
+HAVING COUNT(DISTINCT product_category) = 
+          (SELECT COUNT(DISTINCT product_category) FROM products)
+;
